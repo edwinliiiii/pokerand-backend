ebb9c2200792fe2ba34fb9d66b041745924bfaaf
Li <li.edwin42@gmail.com>
2023-01-10 13:15:11 -0500

    Initial commit, initialized app files and type images

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    template: '<h1> {{ name }} </h1>',
+    data() {
+        return {
+            name: 'Bulbasaur'
+            type: 
+        }
+    }
+})
+
+app.mount('#app')
